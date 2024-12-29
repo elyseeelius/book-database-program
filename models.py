27d501462b78b5f9d66b9c7d588cd587396c40bd
elyseeelius <elyseeelius@outlook.com>
2024-12-29 17:07:55 -0500

    added csv to db

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import (create_engine,Column,
-                        Integer, String, Date)
+from sqlalchemy import (create_engine,Column,Integer, String, Date)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import  sessionmaker
 
